84010c4ff99585aa79e9afc70a6c1e6e03904363
Jonathan Lind <jonathan.m.lind@gmail.com>
2024-04-23 16:08:50 -0500

    harpoon config

--- nvim/init.lua
+++ nvim/init.lua
@@ -327,6 +327,13 @@ require("lazy").setup({
 		"ThePrimeagen/harpoon",
 		branch = "harpoon2",
 		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon:setup()
+			vim.keymap.set("n", "<leader>a", function()
+				harpoon:list():append()
+			end)
+		end,
 	},
 
 	{ -- LSP Configuration & Plugins
