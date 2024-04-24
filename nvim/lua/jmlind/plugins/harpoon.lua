9a17c69e070c34016284faae09e73c8d3b8907ad
Jonathan Lind <jonathan.m.lind@gmail.com>
2024-04-24 11:26:23 -0500

    some more formatting

--- nvim/lua/jmlind/plugins/harpoon.lua
+++ nvim/lua/jmlind/plugins/harpoon.lua
@@ -1,7 +1,7 @@
 return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
-	dependencies = { "nvimlua/plenary.nvim" },
+	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		local harpoon = require("harpoon")
 		harpoon:setup()
