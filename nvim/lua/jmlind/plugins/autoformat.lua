3c44adf700b61db9c7557411429571defa2ca33e
Jonathan Lind <jonathan.m.lind@gmail.com>
2024-11-20 10:46:37 -0600

    update some config for nvim

--- nvim/lua/jmlind/plugins/autoformat.lua
+++ nvim/lua/jmlind/plugins/autoformat.lua
@@ -17,7 +17,7 @@ return { -- Autoformat
 			-- Disable "format_on_save lsp_fallback" for languages that don't
 			-- have a well standardized coding style. You can add additional
 			-- languages here or re-enable it for the disabled ones.
-			local disable_filetypes = { c = true, cpp = true }
+			local disable_filetypes = { c = true, cpp = true, ruby = true }
 			return {
 				timeout_ms = 500,
 				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -30,7 +30,9 @@ return { -- Autoformat
 			--
 			-- You can use a sub-list to tell conform to run *until* a formatter
 			-- is found.
-			-- javascript = { { "prettierd", "prettier" } },
+			javascript = { { "prettierd", "prettier" } },
+			typescript = { { "prettierd", "prettier" } },
+			typescriptreact = { { "prettierd", "prettier" } },
 		},
 	},
 }
--- nvim/lua/jmlind/plugins/git-conflict.lua
+++ nvim/lua/jmlind/plugins/git-conflict.lua
@@ -1,3 +1,12 @@
 return {
-	{ "akinsho/git-conflict.nvim", version = "2", config = true },
+	"akinsho/git-conflict.nvim",
+	version = "2",
+	config = function()
+		require("which-key").add({
+			{ "co", desc = "Git Conflict: [C]hoose [O]urs" },
+			{ "ct", desc = "Git Conflict: [C]hoose [T]heirs" },
+			{ "cb", desc = "Git Conflict: [C]hoose [B]oth" },
+			{ "c0", desc = "Git Conflict: [C]hoose n[0]ne" },
+		})
+	end,
 }
