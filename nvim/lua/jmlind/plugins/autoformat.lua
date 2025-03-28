207f6a7507de467bcb1c2c766206234fa423861d
Jonathan Lind <jonathan.m.lind@gmail.com>
2025-03-27 20:02:09 -0500

    update autoformat to new rules, add nixfmt

--- nvim/lua/jmlind/plugins/autoformat.lua
+++ nvim/lua/jmlind/plugins/autoformat.lua
@@ -30,9 +30,10 @@ return { -- Autoformat
 			--
 			-- You can use a sub-list to tell conform to run *until* a formatter
 			-- is found.
-			javascript = { { "prettierd", "prettier" } },
-			typescript = { { "prettierd", "prettier" } },
-			typescriptreact = { { "prettierd", "prettier" } },
+			javascript = { "prettierd" },
+			typescript = { "prettierd" },
+			typescriptreact = { "prettierd" },
+			nix = { "nixfmt" },
 		},
 	},
 }
