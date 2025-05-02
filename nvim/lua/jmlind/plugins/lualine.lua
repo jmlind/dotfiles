5475c51dc0bc030eb75acb305a89f3574c0e13c9
Jonathan Lind <jonathan.m.lind@gmail.com>
2025-05-02 12:11:13 -0500

    removed lualine config conflicts with base nvim theme, updated catppuccin

--- nvim/lua/jmlind/plugins/lualine.lua
+++ nvim/lua/jmlind/plugins/lualine.lua
@@ -1,12 +1,4 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "catppuccin",
-				-- ... the rest of your lualine config
-			},
-		})
-	end,
 }
