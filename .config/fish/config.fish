6fe35e01355bc1a86626ba7b9e30aba1bac158d6
Jonathan Lind <jonathan.m.lind@gmail.com>
2025-05-06 16:49:53 -0500

    fish config only includes homebrew bin on darwin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,11 +3,18 @@
 
 set -x OBJC_DISABLE_INITIALIZE_FORK_SAFETY YES
 set -x EDITOR nvim
+set -p PATH $HOME/bin $ASDF_DATA_DIR/shims
 
-alias vim "nvim $argv"
+if uname -s | string match -q "Darwin"
+	set -p PATH /opt/homebrew/bin
+end
+
+alias vim "nvim"
+
 
 eval (direnv hook fish)
 
 starship init fish | source
 
 source ~/.asdf/asdf.fish
+
