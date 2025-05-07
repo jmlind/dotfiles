b28f30c9ec8a6b27238c53d858e8e29d3db8306f
Jonathan Lind <jonathan.m.lind@gmail.com>
2025-05-07 10:17:17 -0500

    add some eza aliases and a fish sourcing function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,14 +3,16 @@
 
 set -x OBJC_DISABLE_INITIALIZE_FORK_SAFETY YES
 set -x EDITOR nvim
-set -p PATH $HOME/bin $ASDF_DATA_DIR/shims
+set -p PATH $HOME/bin $ASDF_DATA_DIR/shims /run/current-system/sw/bin
 
 if uname -s | string match -q "Darwin"
 	set -p PATH /opt/homebrew/bin
 end
 
 alias vim "nvim"
-
+alias ll "eza -alh"
+alias lt "eza -alhT"
+alias ls "eza -a"
 
 eval (direnv hook fish)
 
